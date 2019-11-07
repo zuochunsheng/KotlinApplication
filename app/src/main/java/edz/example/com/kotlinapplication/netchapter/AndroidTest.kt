27f72360fe4e4f625e1fd8404cf9a643d3e56d21
zuochunsheng <zuochunsheng@ledcas.com>
2019-11-07 17:12:01 +0800

    kotlin-RxLifecycle导入

--- app/src/main/java/edz/example/com/kotlinapplication/netchapter/AndroidTest.kt
+++ app/src/main/java/edz/example/com/kotlinapplication/netchapter/AndroidTest.kt
@@ -1,12 +1,13 @@
 package edz.example.com.kotlinapplication.netchapter
 
 import io.reactivex.rxkotlin.subscribeBy
+import io.reactivex.rxkotlin.toFlowable
 import io.reactivex.rxkotlin.toObservable
 
 /**
  * @author： zcs
  * @time：2019/11/7 on 16:21
- * @description：
+ * @description：https://github.com/ReactiveX/RxKotlin
  */
 
 
@@ -18,8 +19,16 @@ fun main(args: Array<String>) {
             .filter { it.length >= 5 }
             .subscribeBy(  // named arguments for lambda Subscribers
                     onNext = { println(it) },
-                    onError =  { it.printStackTrace() },
+                    onError = { it.printStackTrace() },
                     onComplete = { println("Done!") }
             )
+
+//    list.toFlowable()
+//            .filter({ it.length >= 5 })
+//            .subscribeBy(
+//                onError = { it.printStackTrace()},
+//                onComplete = { println("Done!")},
+//                onNext = {println(it) }
+//            )
 
 }
