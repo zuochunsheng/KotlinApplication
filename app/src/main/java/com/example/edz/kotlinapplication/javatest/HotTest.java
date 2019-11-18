07577bc9a11a0ee4e1bd5f3f55d3ae9d204cff28
zuochunsheng <zuochunsheng@ledcas.com>
2019-11-18 18:08:49 +0800

    Rx

---
+++ app/src/main/java/com/example/edz/kotlinapplication/javatest/HotTest.java
@@ -0,0 +1,21 @@
+package com.example.edz.kotlinapplication.javatest;
+
+/**
+ * @author： zcs
+ * @time：2019/11/18 on 17:37
+ * @description：
+ */
+public class HotTest {
+
+    public static void main(String... args){
+
+        test();
+
+    }
+
+    private static void test(){
+
+
+    }
+
+}
